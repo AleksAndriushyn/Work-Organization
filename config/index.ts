e3cf076d1af4519abf80b7113c773ba4b3d3465a
Алексей Андрюшин <aleksroebuck@gmail.com>
2023-01-19 16:06:06 +0200

    changed config

--- config/index.ts
+++ config/index.ts
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== 'production'
 
 export const server = dev
   ? 'http://localhost:3000'
-  : 'https://work-organization-wkju.vercel.app'
+  : 'https://work-organization.vercel.app'
